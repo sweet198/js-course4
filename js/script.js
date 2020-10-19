8f55424567b1519901bc22144cde835b3d14d23e
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-10-19 19:44:21 +0300

    in slider changed method slice to replace with patterns

--- js/script.js
+++ js/script.js
@@ -428,10 +428,10 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   sliderNext.addEventListener('click', () => {
-    if (offset === +width.slice(0,width.length - 2) * (slides.length - 1)) {
+    if (offset === +width.replace(/\D/g, '') * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.slice(0,width.length - 2);
+      offset += +width.replace(/\D/g, '');
     }
     slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -455,9 +455,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   sliderPrev.addEventListener('click', () => {
     if (offset === 0) {
-      offset = +width.slice(0,width.length - 2) * (slides.length - 1);
+      offset = +width.replace(/\D/g, '') * (slides.length - 1);
     } else {
-      offset -= +width.slice(0,width.length - 2);
+      offset -= +width.replace(/\D/g, '');
     }
     slidesField.style.transform = `translateX(-${offset}px)`;
 
